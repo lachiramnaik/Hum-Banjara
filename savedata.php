4f7fb696f94dc6a06d72b6c61dac61b4cab17426
lachiram <75107173+lachiram@users.noreply.github.com>
2021-06-16 12:44:45 +0530

    updates

--- savedata.php
+++ savedata.php
@@ -1,6 +1,6 @@
 <?php
-echo $banjara_name=$_POST['sname'];
-
+echo $banjara_name=$_POST['name'];
+echo $banjara_number=$_POST['number'];
 echo $banjara_mail=$_POST['mail'];
 echo $banjara_password=$_POST['password'];
 echo $banjara_country=$_POST['country'];
@@ -8,12 +8,11 @@ echo $banjara_state=$_POST['state'];
 echo $banjara_photo=$_POST['photo'];
 
 $conn=mysqli_connect("localhost","root","","banjara") or die("conection failed"); //connection start
-$sql="INSERT INTO hum-banjara(FULL NAME,MAIL ID,PASSWORD,COUNTRY,STATE,PHOTO) VALUES('{$banjara_name}','{$banjara_mail}','{$banjara_password}','{$banjara_country}','{$banjara_state}',
-'{$banjara_photo}')"; //connetion values insert cheyadam
+$sql="INSERT INTO humbanjara(FULL NAME,MAIL ID,PASSWORD,COUNTRY,STATE,PHOTO,MOBILE NUMBER) VALUES('{$banjara_name}','{$banjara_number}','{$banjara_mail}','{$banjara_password}','{$banjara_country}','{$banjara_state}','{$banjara_photo}')"; //connetion values insert cheyadam
 $result=mysqli_query($conn, $sql) or die("query unsucssful"); //query connection linking
 
 header("Location: http://localhost/Hum-Banjara/index.html");
 
-mysqli_close($conn);
+mysqli_close($conn);  //close the connection
 
 ?>
